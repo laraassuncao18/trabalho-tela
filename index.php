f7219266f8ac03a58bd349f91643dd58e6e05594
Lara <lara.assuncao@alunoescolassociais.org.br>
2024-06-04 12:26:53 -0300

    adicionando cor

--- index.php
+++ index.php
@@ -6,8 +6,36 @@
     <title>Página Principal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
+<style>
+    body {
+    padding-top: 3.5rem;
+}
+
+.navbar-brand {
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+
+h1 {
+    margin-top: 20px;
+    font-size: 2.5rem;
+}
+
+.form-label {
+    font-weight: bold;
+}
+
+.container {
+    max-width: 600px;
+}
+
+.btn-primary {
+    background-color: #ddecc0;
+    border-color: #a7c2df;
+}
+</style>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container-fluid">
